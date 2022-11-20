b4dea6b702191d4f4f2147358175df3d6a11f490
Leonard-McCook <leonard.mccookcarr@outlook.com>
2022-11-20 18:21:46 -0500

    Apply darkerGray background color and add geometry reader

--- CalculatorApp/CalculatorHome.swift
+++ CalculatorApp/CalculatorHome.swift
@@ -14,37 +14,43 @@ let darkGray = Color(CGColor(gray: 0.3, alpha: 1))
 
 struct CalculatorHome: View {
     var body: some View {
-        VStack(alignment: .trailing, spacing: 0) {
-            
-            Spacer()
-            
-            // Display the current value
-            Text("0")
-                .foregroundColor(Color.white)
-                .font(.system(size: 40))
-                .lineLimit(1)
-                .padding(.leading, 20)
-                .padding(.trailing, 20)
-            
-            // Display the rows of buttons with specified labels
-            VStack(spacing: 10) {
-                CalculatorRow(labels: ["CE", "",
-                                       "", String("\u{00f7}")],
-                              colors: [darkGray, darkGray,
-                                       darkGray, .orange])
+        GeometryReader { geometry in
+                VStack(alignment: .trailing, spacing: 0) {
+                    
+                    Spacer()
+                    
+                    // Display the current value
+                    Text("0")
+                        .font(.system(size: 85))
+                        .foregroundColor(Color.white)
+                        .lineLimit(1)
+                        .padding(.leading, 20)
+                        .padding(.trailing, 20)
+                    
+                    // Display the rows of buttons with specified labels
+                    VStack(spacing: 10) {
+                        CalculatorRow(labels: ["CE", "",
+                                               "", String("\u{00f7}")],
+                                      colors: [darkGray, darkGray,
+                                               darkGray, .orange])
+                        
+                        
+                        CalculatorRow(labels: ["7", "8", "9", String("\u{00d7}")])
+                        
+                        CalculatorRow(labels: ["4", "5", "6","-"])
+                        
+                        CalculatorRow(labels: ["1", "2", "3", "+"])
+                        
+                        CalculatorRow(labels: ["0", ".", "", "="])
+                        
+                    }
+                    .frame(height: geometry.size.height
+                           * 0.7)
                 
-                
-                CalculatorRow(labels: ["7", "8", "9", String("\u{00d7}")])
-                
-                CalculatorRow(labels: ["4", "5", "6","-"])
-                
-                CalculatorRow(labels: ["1", "2", "3", "+"])
-                
-                CalculatorRow(labels: ["0", ".", "", "="])
-                
+                    .padding()
             }
         }
-        
+        .background(darkerGray)
     }
 }
 
