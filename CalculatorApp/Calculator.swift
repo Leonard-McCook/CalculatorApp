a42e63c0b4c62d15a1700fa0dc9462fac72d807d
Leonard-McCook <leonard.mccookcarr@outlook.com>
2022-11-21 17:07:49 -0500

    Create operator file and add variables

--- CalculatorApp/Calculator.swift
+++ CalculatorApp/Calculator.swift
@@ -7,7 +7,23 @@
 
 import Foundation
 
-
 class Calculator: ObservableObject {
     
+  // Used to update the UI
+    @Published var displayValue = "0"
+    
+    // Store thwe current operator
+    var currentOP: Operator?
+    
+    // Cureent number selected
+    var currentNumber: Double? = 0
+    
+    // Previous number selected
+    var previousNumber: Double?
+    
+    // Flag for equal press
+    var equaled = false
+    
+    // How many decimal places have been typed
+    var decimalPlace = 0
 }
---
+++ CalculatorApp/Operator.swift
@@ -0,0 +1,13 @@
+//
+//  Operator.swift
+//  CalculatorApp
+//
+//  Created by Leonard McCook-Carr on 11/21/22.
+//
+
+import Foundation
+
+
+class Operator {
+    
+}
